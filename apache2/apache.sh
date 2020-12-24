6fb57b491a14d6599b0e59b20c9934d261d69cb4
Aman soni <amansoniabesgmail.com>
2020-12-24 06:09:15 +0530

    save changed

--- apache2/apache.sh
+++ apache2/apache.sh
@@ -3,11 +3,12 @@
 
 sudo apt-get update -y
 sudo apt-get install apache2 
+sudo nano /var/www/html/index.html
+sudo nano /etc/apache2/sites-available/000-default.conf
+
 sudo systemctl start apache2
 sudo ufw app list
 
-sudo nano /etc/apache2/ports.conf
-sudo nano /etc/apache2/sites-available/000-default.conf
 sudo systemctl restart apache2
 
 sudo nano /etc/apache2/apache2.conf
